b900a103c3b2e96e0d647129fff772790389b1cb
Yuyang-SBKF <yuyang@capinfo.com.cn>
2019-02-18 18:13:06 +0800

    修改代码规范

--- yumi-mall-admin/src/main/java/com/yumi/mall/config/Swagger2Config.java
+++ yumi-mall-admin/src/main/java/com/yumi/mall/config/Swagger2Config.java
@@ -6,12 +6,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger2API文档的配置
+ * http://ip:port/swagger-ui.html
  */
 @Configuration
 @EnableSwagger2
@@ -27,11 +29,16 @@ public class Swagger2Config {
     }
 
     private ApiInfo apiInfo() {
+        Contact contact=new Contact("yuyumimi",
+                "https://github.com/yuyumimi/yumi-mall","mendandan@yeah.net");
         return new ApiInfoBuilder()
                 .title("yumi-mall后台系统")
                 .description("yumi-mall后台模块")
-                .contact("yumi")
+                .contact(contact)
+                .license("Apache License 2.0")
+                .licenseUrl("https://github.com/yuyumimi/yumi-mall/blob/master/LICENSE")
                 .version("1.0")
                 .build();
+
     }
 }
