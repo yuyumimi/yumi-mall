9b2f49fd6ec056ffd3c2188654532bf26c8abd4a
Yuyang-SBKF <yuyang@capinfo.com.cn>
2019-03-10 18:06:46 +0800

    增加搜索模块

---
+++ yumi-mall-search/src/main/java/com/yumi/mall/search/config/Swagger2Config.java
@@ -0,0 +1,44 @@
+package com.yumi.mall.search.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2API文档的配置
+ * http://ip:port/swagger-ui.html
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yumi.mall.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact=new Contact("yuyumimi",
+                "https://github.com/yuyumimi/yumi-mall","mendandan@yeah.net");
+        return new ApiInfoBuilder()
+                .title("yumi-mall搜索系统")
+                .description("yumi-mall搜索模块")
+                .contact(contact)
+                .license("Apache License 2.0")
+                .licenseUrl("https://github.com/yuyumimi/yumi-mall/blob/master/LICENSE")
+                .version("1.0")
+                .build();
+
+    }
+}
